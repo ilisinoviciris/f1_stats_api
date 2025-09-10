3e5a98fac04b9f3e147049cb262f2c24ab7c6f3f
Iris Ilisinovic <iris.ilisinovic@gmail.com>
2025-09-10 18:53:11 +0200

    feat: add database config with sqlalchemy

---
+++ database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+# SQLite connection URL
+DATABASE_URL = "sqlite:///./f1_stats.db"
+
+# connection to the database
+engine = create_engine (DATABASE_URL, connect_args={"check_same_thread": False})
+
+# session factory for database operations (CRUD)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# base class for all ORM models
+Base = declarative_base()
