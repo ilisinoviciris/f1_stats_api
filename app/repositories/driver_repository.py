4593f12fad13ce4e80d15e69684e61c14b40ec4e
Iris Ilisinovic <iris.ilisinovic@gmail.com>
2025-09-18 20:37:19 +0200

    docs: update comments

--- app/repositories/driver_repository.py
+++ app/repositories/driver_repository.py
@@ -1,7 +1,6 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
-from .. import models, schemas
-import httpx
+from app import models, schemas
 
 # return all drivers from the database
 def get_all_drivers(db: Session):
