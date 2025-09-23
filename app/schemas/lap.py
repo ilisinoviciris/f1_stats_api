f04003a314c8c8816ad10f8eef26038313bc29fe
Iris Ilisinovic <iris.ilisinovic@gmail.com>
2025-09-23 20:55:46 +0200

    refactor: remove session_name and session_type

--- app/schemas/lap.py
+++ app/schemas/lap.py
@@ -4,6 +4,7 @@ from typing import Optional
 # fields for Lap
 class LapBase(BaseModel):
     race_id: int
+    session_id: int
     driver_number: int
     lap_number: int
     lap_duration: Optional[float] = None
@@ -14,17 +15,21 @@ class LapBase(BaseModel):
     i2_speed: Optional[float] = None
     st_speed: Optional[float] = None
     is_pit_out_lap: Optional[bool] = None
-    session_id: int
-    session_name: Optional[str] = None
-    session_type: Optional[str] = None
 
 # fields to create a new lap
 class LapCreate(LapBase):
     pass
 
 # fields for updating a driver (all fields are optional)
-class LapUpdate(LapBase):
-    pass
+class LapUpdate(BaseModel):
+    lap_duration: Optional[float] = None
+    duration_sector_1: Optional[float] = None
+    duration_sector_2: Optional[float] = None
+    duration_sector_3: Optional[float] = None
+    i1_speed: Optional[float] = None
+    i2_speed: Optional[float] = None
+    st_speed: Optional[float] = None
+    is_pit_out_lap: Optional[bool] = None
 
 # fields returned
 class Lap(LapBase):
