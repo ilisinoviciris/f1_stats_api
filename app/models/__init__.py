31c12efb9401574ead875d88327b80fdaaf205c8
Iris Ilisinovic <iris.ilisinovic@gmail.com>
2025-09-23 20:53:30 +0200

    chore: update __init__.py files

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,5 +5,6 @@ Base = declarative_base()
 from .driver import Driver
 from .race import Race
 from .lap import Lap
+from .session import Session
 
-__all__ = ["Driver", "Race", "Lap"]
+__all__ = ["Driver", "Race", "Lap", "Session"]
--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,3 +1,4 @@
 from .driver import Driver, DriverCreate, DriverUpdate
 from .race import Race, RaceCreate, RaceUpdate
 from .lap import Lap, LapCreate, LapUpdate
+from .session import Session, SessionCreate, SessionUpdate
